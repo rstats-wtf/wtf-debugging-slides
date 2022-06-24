e30b6646467ddc1f67061350215a01ce339354ac
Shannon Pileggi <pileggis@mskcc.org>
2022-06-24 15:11:48 -0400

    updates

---
+++ demo/error_inspector.R
@@ -0,0 +1,15 @@
+# IDE Error Inspector not triggered
+f <- function(x) x + 1
+g <- function(x) f(x)
+g("a")
+
+# IDE Error Inspector not triggered
+strsplit(factor("a,b"), ",")
+
+# IDE Error Inspector not triggered
+f <- function(x) strsplit(x, ",")
+f(factor("a,b"))
+
+# IDE Error Inspector yes triggered
+g <- function(x) f(x)
+g(factor("a,b"))
