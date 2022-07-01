97d4139587eb56c26f7d81bf5b7a67d7fe151db9
Shannon Pileggi <pileggis@mskcc.org>
2022-06-30 21:54:35 -0400

    so close!

---
+++ demo/error_recover.R
@@ -0,0 +1,6 @@
+source("demo/my_functions.R")
+options(error = recover)
+g("a")
+options(error = NULL)
+
+
--- demo/warning.R
+++ demo/warning.R
@@ -1,1 +1,9 @@
-options(warn = 0)
+options(warn = 0) # default, stores warnings until top-level function returns
+options(warn = 1) # warnings are printed as they occur
+options(warn = 2) # upgrades warnings to errors
+
+
+# initiate recover on warning
+options(warn = 2, error = recover)
+# restore original settings
+options(warn = 0, error = NULL)
