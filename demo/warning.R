327cfc2ed314819351adb8af78b9e28047ee6fc0
Shannon Pileggi <pileggis@mskcc.org>
2022-07-10 22:56:51 -0400

    add concept overview

--- demo/warning.R
+++ demo/warning.R
@@ -1,9 +1,3 @@
-options(warn = 0) # default, stores warnings until top-level function returns
-options(warn = 1) # warnings are printed as they occur
-options(warn = 2) # upgrades warnings to errors
-
-
-# initiate recover on warning
-options(warn = 2, error = recover)
-# restore original settings
-options(warn = 0, error = NULL)
+options(warn = 0) # default, warnings deferred to end
+options(warn = 1) # print warning when it happens
+options(warn = 2) # converts all warnings to errors, allows you to use regular debugging tools
